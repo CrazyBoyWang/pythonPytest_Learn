56a6a7ce17f567a84658a6c04ac04437220666b0
314158893@qq.com <@Wxs8784725>
2022-12-05 19:17:55 +0800

    调整配置文件

--- utils/read_data.py
+++ utils/read_data.py
@@ -6,7 +6,7 @@ import yaml
 path = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))),"config","data.yaml")
 def read_data():
     # 读取文件
-    f = open(path, encoding="utf8")
+    f = open(path,encoding="utf8")
     data = yaml.safe_load(f)
     return data
 
